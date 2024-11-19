c53a3c5c1595ddac1e325cb660f86e304cf37015
gkatsinoulas <gkatsinoulas@cognity.gr>
2024-11-19 12:35:54 +0200

    Add Jakarta Faces Java logic

---
+++ src/main/java/org/eclipse/jakarta/faces/FacesActivator.java
@@ -0,0 +1,10 @@
+package org.eclipse.jakarta.faces;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+ 
+@FacesConfig()
+@ApplicationScoped
+public class FacesActivator {
+     
+}
